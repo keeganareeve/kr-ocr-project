62f071720e03f1a1fc0d927ce9f69d59c2b7334f
Keegan Reeve <115120316+keeganareeve@users.noreply.github.com>
2023-08-08 10:42:13 -0400

    Update crop_image.py

--- crop_image.py
+++ crop_image.py
@@ -1,108 +1,88 @@
-# Packages to import
+'''
+To run:
+    python3 crop_image.py <image_directory> <image_name> <output_filepath>
+            sys.argv[0]   sys.argv[1]       sys.argv[2]  sys.argv[3]
+'''
+
+# STEP 1
+# Possible issue with pytesseract python package: https://stackoverflow.com/questions/33401767/importerror-no-module-named-pytesseract
+
+# Path to your image file
 import sys
-import cv2  # OpenCV, for identifying "structure"
+import os
+
 import pytesseract
+import numpy as np
+import imutils
+import cv2
 
-''''
-# Checking version of OpenCV
-print(f"Version of cv2 package: {cv2.__version__}")
-'''
+# STEP 2
+# '/content/gdrive/MyDrive/ImageFiles/inputForCropping/page66.png'
+image_directory = str(sys.argv[1])
+image_name = str(sys.argv[2])
+output_filepath = str(sys.argv[3])
 
-'''
-Setting arguments.
-'''
-image_filepath = sys.argv[1]
+image_filepath = image_directory+image_name
+original_image = cv2.imread(image_filepath)
+
+# STEP 2
+image_path = image_filepath
+original_image = cv2.imread(image_path)
+
 x_min_distance = 130
 y_min_distance = 150
 
-'''
-Preprocessing.
-'''
-original_image = cv2.imread(image_filepath)
-image_copy = cv2.imread(image_filepath)
+
+original_image = cv2.imread(image_path)
 x_length, y_length, z_length = original_image.shape
 
+# STEP 3
 grayscaled_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-blurred_image = cv2.GaussianBlur(grayscaled_image, (7, 7), 0)
+blurred_image = cv2.GaussianBlur(grayscaled_image, (7, 7), 0)  # 7-by-7 size
 threshold_image = cv2.threshold(
     blurred_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 13))
 after_convolutions = cv2.dilate(threshold_image, kernel, iterations=1)
 contours = cv2.findContours(
     after_convolutions, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# STEP 4
+# Converting the format of the contours
 contours = contours[0] if len(contours) == 2 else contours[1]
 contours = sorted(contours, key=lambda x: cv2.boundingRect(x)[1])
 
+# STEP 5
+# this may be a new idea for a procedure to use for finding lines of images instead of making only one:
+# https://stackoverflow.com/questions/64002869/how-to-combine-bounding-boxes-in-opencv-python
 
-'''
-Getting the contours we're interested in.
-'''
-contours_away_from_margins = []
+rectTuples = []
 for contour in contours:
     xValue, yValue, width, height = cv2.boundingRect(contour)
 
     if height < 200 and xValue in range(y_min_distance, x_length - x_min_distance) and yValue in range(y_min_distance, y_length):
-        contours_away_from_margins.append(contour)
+        # cv2.rectangle(original_image, (xValue, yValue),
+        # (xValue+width, yValue+height), (0, 0, 255), 2)
 
+        rectTuples.append((xValue, yValue, width, height))
 
-'''
-For testing parameters from this script, if necessary.
-'''
-output_file = "temp/boundingBoxes.png"
+# STEP 6
+# MERGE BOXES
+# Then, the same code as in group_bounding_boxes.py in order to merge boxes and crop.
+arr = []
+for x, y, w, h in rectTuples:
+    arr.append((x, y))
+    if x+w > x_length-x_min_distance:  # do these if-statements make any different?
+        if y+h > y_length-y_min_distance:
+            if y-h > y_min_distance:
+                # increases x coord by width of box, and y coord to height of box in order to combine all boxes
+                arr.append((x+w, y+h))
 
+x, y, w, h = cv2.boundingRect(np.asarray(arr))
+image_copy = cv2.imread(image_path)
+cv2.rectangle(
+    image_copy, (x, y), (x+w, y+h), (0, 0, 255), 1)
 
-def testParameters(output_file, ctrs, original_image=original_image):
-    for c in ctrs:
-        xValue, yValue, width, height = cv2.boundingRect(c)
-
-        # if height < 200 and xValue in range(y_min_distance, x_length - x_min_distance) and yValue in range(y_min_distance, y_length):
-        cv2.rectangle(original_image, (xValue, yValue),
-                      (xValue+width, yValue+height), (0, 255, 0), 2)
-    cv2.imwrite(output_file, original_image)
-    print(f"Wrote image to {output_file}.")
-
-
-testParameters(output_file, contours_away_from_margins)  # _away_from_margins)
-
-'''
-Cropping contours from image and saving these contours as images and text.
-'''
-
-
-def getContours(image, ctrs):
-    imageList = []
-    for c in ctrs:
-        xValue, yValue, width, height = cv2.boundingRect(c)
-        # If point starts at top right, it should be '-' instead of '+'
-        imageList.append(image[yValue:yValue + height, xValue:xValue + width])
-    return imageList
-
-
-def saveImages(imageList, directory):
-    i = 0
-    for img in imageList:
-        # if i < 20:
-        output_filename = f"{directory}croppedImage{i}.png"
-        cv2.imwrite(output_filename, img)
-        print(f"Wrote {output_filename} to {directory} directory.")
-        i += 1
-    return None
-
-
-def saveText(string_list, filepath):
-    try:
-        with open(filepath, 'w') as file:
-            for item in string_list:
-                file.write(item + '\n')
-        print("List of strings has been successfully saved to", filepath)
-    except IOError as e:
-        print("Error:", e)
-
-
-croppedImages = getContours(image_copy, contours_away_from_margins)
-saveImages(croppedImages, "temp/cropped_images/")
-
-croppedText = []
-for i, cropped_section in enumerate(croppedImages):
-    croppedText.append(pytesseract.image_to_string(cropped_section))
-saveText(croppedText, "temp/croppedText.txt")
+# STEP 7: Cropping it
+cropped_image = image_copy[y:y+h, x:x+w]
+cv2.imwrite(output_filepath, cropped_image)
+print(f"Saved {output_filepath}")
